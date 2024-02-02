2626e9e42143be0cf75a15c048c5dd6505a135d9
Jonathan Nye <jonnye@tesla.com>
2024-02-02 10:48:52 +0100

    Update favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -80,7 +80,8 @@ export const Layout: React.FC<Props> = ({
             <Head>
                 <link
                     rel="icon"
-                    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💐</text></svg>"
+                    type="image/x-icon"
+                    href="/favicons/favicon.ico"
                 />
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
